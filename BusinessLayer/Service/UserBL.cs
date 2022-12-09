f37a4c13eb2398001cd4d14a68ab9610997135a2
Imransk7768 <imransk7768@gmail.com>
2022-12-09 22:20:01 +0530

    Implemented User Register and Login

---
+++ BusinessLayer/Service/UserBL.cs
@@ -0,0 +1,42 @@
+﻿using BusinessLayer.Interface;
+using CommonLayer.Model;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Service
+{
+    public class UserBL : IUserBL
+    {
+        private readonly IUserRL iuserRL;
+        public UserBL(IUserRL iuserRL)
+        {
+            this.iuserRL = iuserRL;
+        }
+        public UserReg Registration(UserReg userReg)
+        {
+            try
+            {
+                return iuserRL.Registration(userReg);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public UserLogin Login(UserLogin userLogin)
+        {
+            try
+            {
+                return iuserRL.Login(userLogin);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+    }
+}

e6ab5112522be5a7eeed31c592f9ed095b8730ed
Imransk7768 <imransk7768@gmail.com>
2022-12-09 22:20:42 +0530

    Implemented UserModel

---
+++ CommonLayer/Model/UserReg.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CommonLayer.Model
+{
+    public class UserReg
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public long Mobile { get; set; }
+        public string Password { get; set; }
+    }
+}
